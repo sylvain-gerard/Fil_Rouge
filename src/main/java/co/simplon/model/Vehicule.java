359d53d954d8391fba428d657e41a48e07555014
Gerard <sylvaingerard4@hotmail.com>
2018-02-12 15:54:25 +0100

    modif Vehicule

--- src/main/java/co/simplon/model/Vehicule.java
+++ src/main/java/co/simplon/model/Vehicule.java
@@ -1,47 +1,11 @@
 package co.simplon.model;
 
 public class Vehicule {
-	private Long id;
-	private String type;
-	private String marque;
-	private String modele;
+	private Long id_vehicules;
+	private String modele_vehicule;
+	private String marque_vehicule;
 	private String immatriculation;
-	private String chassis;
+	private String couleur_vehicule;
 	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getType() {
-		return type;
-	}
-	public void setType(String type) {
-		this.type = type;
-	}
-	public String getMarque() {
-		return marque;
-	}
-	public void setMarque(String marque) {
-		this.marque = marque;
-	}
-	public String getModele() {
-		return modele;
-	}
-	public void setModele(String modele) {
-		this.modele = modele;
-	}
-	public String getImmatriculation() {
-		return immatriculation;
-	}
-	public void setImmatriculation(String immatriculation) {
-		this.immatriculation = immatriculation;
-	}
-	public String getChassis() {
-		return chassis;
-	}
-	public void setChassis(String chassis) {
-		this.chassis = chassis;
-	}	
+	
 }
