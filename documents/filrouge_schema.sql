4784c6cfda9fc4c31f6f26851624bf90b31fc9d7
Gerard <sylvaingerard4@hotmail.com>
2018-04-10 19:37:20 +0200

    FINAL COMMIT

--- documents/filrouge_schema.sql
+++ documents/filrouge_schema.sql
@@ -1,15 +1,4 @@
--- MySQL Workbench Forward Engineering
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
-
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema fil_rouge
--- -----------------------------------------------------
 
 -- -----------------------------------------------------
 -- Schema fil_rouge
@@ -159,6 +148,3 @@ AUTO_INCREMENT = 5
 DEFAULT CHARACTER SET = utf8;
 
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
